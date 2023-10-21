524c670baae3e42efb624c11f3c03779a3a50cb4
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-20 20:39:16 -0700

    add file generation process

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import * as fs from 'node:fs';
+import Circle from "./lib/shapes.js";
 
 const questions = [
     {
@@ -36,13 +38,32 @@ const questions = [
         name: 'shape_color',
         message: 'Provide the name of the color of the shape or a hexadecimal number'
     },
-]
+];
+
+function outputFile(shapeElement, textElement){
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+${shapeElement}
+${textElement}
+</svg>`
+};
+
+function createFile(fileName,input) {
+    fs.writeFile(fileName,input,(err) =>
+    err ? console.error(err) : console.log('Generated logo.svg'));
+}
 
 function init() {
     inquirer
       .prompt(questions)
       .then((response) => {
-        console.log(response);
+        const newShape = new Circle;
+        newShape.setColor(response.shape_color);
+        newShape.setText(response.text);
+        newShape.setTextColor(response.text_color);
+        const shapeElement = newShape.render();
+        const textElement = newShape.renderText();
+        const svgContent = outputFile(shapeElement,textElement);
+        createFile("./examples/logo.svg",svgContent);
       });
   }
   
